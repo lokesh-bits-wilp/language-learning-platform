c788992f8d912792dec6a7354c50b49b978d471b
Lokesh Maheshwari <lmaheshwari@systango.com>
2024-05-02 12:22:15 +0530

    Add language in dashboard

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CoreBackendService } from 'src/app/services/core-backend.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   email: string = localStorage.getItem('email') || '';
   authToken: string = localStorage.getItem('authToken') || '';
+  language: any
 
   constructor(
-    private router: Router
+    private router: Router,
+    private coreBackendService: CoreBackendService
   ) {
     
+  }
+
+  ngOnInit() {
+    this.loadUserDetail();
+  }
+
+  async loadUserDetail() {
+    const response = await this.coreBackendService.getLanguagesByUser(this.authToken);
+    console.log(response);
+    this.language = response;
   }
 
   async redirectToProfile() {
